28f033823570cb2a3ee2d3d379fa1c9e46de05e0
williamacquah <77838879+williamacquah@users.noreply.github.com>
2026-04-06 23:26:55 +0200

    Add Day 7 Hangman

---
+++ Day7_Hangman/Hangman.py
@@ -0,0 +1,54 @@
+import random
+import hangman_words
+import hangman_art
+
+lives = 6
+
+print(hangman_art.logo)
+chosen_word = random.choice(hangman_words.word_list)
+print(chosen_word)
+
+placeholder = ""
+word_length = len(chosen_word)
+for position in range(word_length):
+    placeholder += "_"
+print("Word to guess: " + placeholder)
+
+game_over = False
+correct_letters = []
+
+while not game_over:
+
+    print(f"****************************<???>{lives}/6 LIVES LEFT****************************")
+    guess = input("Guess a letter: ").lower()
+
+    if guess in correct_letters:
+        print("You've already guessed " + guess)
+    display = ""
+
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+
+    print("Word to guess: " + display)
+
+
+    if guess not in chosen_word:
+        lives -= 1
+        print("You've already guessed " + guess + ", that's not in the word. You lose a life.")
+
+        if lives == 0:
+            game_over = True
+
+            print(f"IT WAS {chosen_word}! YOU LOSE")
+
+    if "_" not in display:
+        game_over = True
+        print("****************************YOU WIN****************************")
+
+    print(hangman_art.stages[lives])
